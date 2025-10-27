70920bd98c8bf4c931d4dd95ac98bb569a878fb7
janishuber <janis.huber@itme.ch>
2025-10-27 15:52:18 +0100

    mock-data

--- finpath-frontend/src/mocks/handlers.ts
+++ finpath-frontend/src/mocks/handlers.ts
@@ -1,10 +1,12 @@
 import { http, HttpResponse } from 'msw';
 import userdata from './data/user.data.json';
 import checkindata from './data/check-in.data.json';
+import financialSummaryData from './data/financial-management.data.json';
 
 export const handlers = [
   http.get('/api/v1/user/data', () => HttpResponse.json(userdata)),
-  http.get('/api/v1/dashboard/checkin', () => HttpResponse.json(checkindata)),
+  http.get('/api/v1/dashboard/check-in', () => HttpResponse.json(checkindata)),
+  http.get('api/v1/dashboard/monthly-summary', () => HttpResponse.json(financialSummaryData)),
   // weitere mocks:
   // http.get('/api/v1/learning/progress', () => HttpResponse.json({...})),
   // http.post('/api/v1/goals', async ({ request }) => {
