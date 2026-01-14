2eb42957bb6cfd866b73a8ddbc675cb474eb6d25
janishuber <janis.huber@itme.ch>
2026-01-14 21:10:46 +0100

    Minimal Frontend Example for Auth & Onboarding Workflow

--- apps/finpath-frontend-web/src/app/core/routing/app.routes.ts
+++ apps/finpath-frontend-web/src/app/core/routing/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
-
+import { AUTH_ROUTES } from '@features/auth/routes';
 
 export const APP_ROUTES: Routes = [
-  { path: 'dashboard', 
+  ...AUTH_ROUTES,
+  { path: 'dashboard',
     loadChildren: () => import('@features/dashboard/routes').then(m => m.DASHBOARD_ROUTES),
     title: 'Dashboard',
     data: { icon: 'dashboard', breadcrumb: 'Dashboard' }
