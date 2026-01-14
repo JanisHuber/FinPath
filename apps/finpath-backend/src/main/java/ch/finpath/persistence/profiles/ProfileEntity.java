e6e1965b757ba52f1b3ace0d1a9e93dbf0863ff7
janishuber <janis.huber@itme.ch>
2026-01-14 21:37:32 +0100

    no updatet_at for profiles table

--- apps/finpath-backend/src/main/java/ch/finpath/persistence/profiles/ProfileEntity.java
+++ apps/finpath-backend/src/main/java/ch/finpath/persistence/profiles/ProfileEntity.java
@@ -5,7 +5,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -24,10 +23,6 @@ public class ProfileEntity {
     @CreationTimestamp
     @Column(name = "created_at", nullable = false, updatable = false)
     private OffsetDateTime createdAt;
-
-    @UpdateTimestamp
-    @Column(name = "updated_at", nullable = false)
-    private OffsetDateTime updatedAt;
 
     public ProfileEntity() {
     }
@@ -59,13 +54,5 @@ public class ProfileEntity {
 
     public void setCreatedAt(OffsetDateTime createdAt) {
         this.createdAt = createdAt;
-    }
-
-    public OffsetDateTime getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(OffsetDateTime updatedAt) {
-        this.updatedAt = updatedAt;
     }
 }
