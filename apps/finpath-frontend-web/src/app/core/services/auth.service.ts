1b8bbcfd4ec89c0add742182b50e429b6028cb20
janishuber <janis.huber@itme.ch>
2026-01-16 14:50:16 +0100

    Profile loading

--- apps/finpath-frontend-web/src/app/core/services/auth.service.ts
+++ apps/finpath-frontend-web/src/app/core/services/auth.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { environment } from '@env';
 import { AuthChangeEvent, AuthResponse, createClient, Session, SupabaseClient, User } from '@supabase/supabase-js';
 import { BehaviorSubject, from, Observable } from 'rxjs';
+import { Profile } from '@models/profile.models'
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
@@ -9,15 +11,22 @@ import { BehaviorSubject, from, Observable } from 'rxjs';
 export class AuthService {
   private supabase: SupabaseClient;
   private currentUserSubject: BehaviorSubject<User | null>;
+  private currentProfileSubject: BehaviorSubject<Profile | null>;
   private initializedSubject = new BehaviorSubject<boolean>(false);
 
   public currentUser$: Observable<User | null>;
+  public currentProfile$: Observable<Profile | null>;
   public isInitialized$ = this.initializedSubject.asObservable();
 
-  constructor() {
+  private http: HttpClient;
+
+  constructor(private readonly httpClient: HttpClient) {
+    this.http = httpClient;
     this.supabase = createClient(environment.supabaseUrl, environment.supabaseKey);
     this.currentUserSubject = new BehaviorSubject<User | null>(null);
     this.currentUser$ = this.currentUserSubject.asObservable();
+    this.currentProfileSubject = new BehaviorSubject<Profile | null>(null);
+    this.currentProfile$ = this.currentProfileSubject.asObservable();
 
     this.initializeAuth();
   }
@@ -29,6 +38,17 @@ export class AuthService {
 
     this.supabase.auth.onAuthStateChange((_event: AuthChangeEvent, session: Session | null) => {
       this.currentUserSubject.next(session?.user ?? null);
+      if (session?.user) {
+        this.http.get<Profile>(`${environment.apiBaseUrl}/me`).subscribe({
+          next: (profile) => {
+            this.currentProfileSubject.next(profile);
+            console.log("User Profile loaded: " + JSON.stringify(profile));
+          },
+          error: (err) => console.error("Failed to load profile:", err)
+        });
+      } else {
+        this.currentProfileSubject.next(null);
+      }
     });
   }
 
@@ -68,6 +88,10 @@ export class AuthService {
 
   getCurrentUser(): User | null {
     return this.currentUserSubject.value;
+  }
+
+  getCurrentProfile(): Profile | null {
+    return this.currentProfileSubject.value;
   }
 
   isAuthenticated(): boolean {
