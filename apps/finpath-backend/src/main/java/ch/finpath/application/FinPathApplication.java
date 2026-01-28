31c09d8771202f77400e2549d6de395e0490a033
janishuber <janis.huber@itme.ch>
2026-01-28 10:11:34 +0100

    Debug on dev

--- apps/finpath-backend/src/main/java/ch/finpath/application/FinPathApplication.java
+++ apps/finpath-backend/src/main/java/ch/finpath/application/FinPathApplication.java
@@ -2,11 +2,13 @@ package ch.finpath.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 
 @SpringBootApplication
 public class FinPathApplication {
-
     public static void main(String[] args) {
+        System.out.println(System.getenv("JDBC_URL"));
+        System.out.println(System.getenv("SUPABASE_AUTH_URL"));
         SpringApplication.run(FinPathApplication.class, args);
     }
 }
